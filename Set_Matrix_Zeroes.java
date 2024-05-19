b697b0658dc322550308e1d0e53701e30e051925
shreyas-0203 <bhosaleshreyas53@gmail.com>
2024-05-19 13:16:22 +0530

    Set Matrix zeros

--- Set_Matrix_Zeroes.java
+++ Set_Matrix_Zeroes.java
@@ -36,4 +36,21 @@ public class Set_Matrix_Zeroes
 }
 
 
+/*
 
+Brute Force :
+            1) Traverse when u find zero Mark row and mark col to -1 EXCEPT 0.
+            2) After that traverse again and re-mark -1 to Zero.
+
+            3) Time Complexity = O(n^3)
+
+Optimal Solution:
+            1) Make one row-sized-array and col-sized-array to keep track location o zeros in matrix.
+            2) When arr[i][j]==0 ,row array or col-array index sets to 1
+            3) At last just re traverse the array and check if row/col array is sets or not
+                if sets then arr[i][j] = 0 else remain 1.
+
+            4) Time Complexity = O(2*n*m).
+            5) Space Complexity = O(m) + O(n).
+
+ */
